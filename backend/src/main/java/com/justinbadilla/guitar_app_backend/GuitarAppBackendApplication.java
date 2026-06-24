ce8a6827b5446fb815f47661a860c5438233d1fb
justinbadilla <jbadilla@sfsu.edu>
2026-06-23 23:13:27 -0700

    Add Spring Boot backend

---
+++ backend/src/main/java/com/justinbadilla/guitar_app_backend/GuitarAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.justinbadilla.guitar_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarAppBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/justinbadilla/guitar_app_backend/GuitarAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justinbadilla.guitar_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitarAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
